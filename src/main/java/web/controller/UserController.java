dff3dab6a5f66bd9509d191e937c77a5af93c23c
AL <alexander.lastovs@gmail.com>
2025-10-28 03:20:57 +0300

    first

---
+++ src/main/java/web/controller/UserController.java
@@ -0,0 +1,59 @@
+package web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import web.model.User;
+import web.service.UserService;
+
+@Controller
+@RequestMapping
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+    @GetMapping("/users")
+    public String list(Model model) {
+        model.addAttribute("users", userService.findAll());
+        return "index";
+    }
+    @GetMapping("/new")
+    public String newUser(Model model) {
+        model.addAttribute("user", new User());
+        return "newUser";
+    }
+    @GetMapping("/edit")
+    public String editUser(@RequestParam("id") Long id, Model model){
+        User user = userService.findById(id);
+        model.addAttribute("user", user);
+        return "newUser";
+    }
+    @PostMapping
+    public String saveUser(@ModelAttribute("user") User user){
+        userService.save(user);
+        return "redirect:/users";
+    }
+    @PostMapping("/update")
+    public String updateUser(@ModelAttribute("user") User user){
+        userService.update(user);
+        return "redirect:/users";
+    }
+    @PostMapping("/delete")
+    public String deleteUser(@RequestParam("id") Long id){
+        userService.delete(id);
+        return "redirect:/users";
+    }
+    @GetMapping("/")
+    public String redirectToUsers() {
+        return "redirect:/users";
+    }
+}
---
+++ src/main/java/web/model/User.java
@@ -0,0 +1,100 @@
+package web.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Objects;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "age")
+    private Integer age;
+
+    @Column(name = "city")
+    private String city;
+
+    public User() {}
+    public User(String name, String email, Integer age, String city) {
+        this.name = name;
+        this.email = email;
+        this.age = age;
+        this.city = city;
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(Id, user.Id) && Objects.equals(name, user.name) && Objects.equals(email, user.email) && Objects.equals(age, user.age) && Objects.equals(city, user.city);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(Id, name, email, age, city);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "Id=" + Id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", age=" + age +
+                ", city='" + city + '\'' +
+                '}';
+    }
+}
